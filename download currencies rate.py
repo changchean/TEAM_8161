2a645e62a0398e719f158901a5378b1808c59bed
TEAM_8161 <andy14337@gmail.com>
2025-11-15 13:52:40 +0800

    Update download currencies rate.py

--- download currencies rate.py
+++ download currencies rate.py
@@ -3,13 +3,15 @@ import requests
 
 acct_transaction = pd.read_csv('/mnt/d/AI CUP/DATA/初賽資料/acct_transaction.csv')
 currency_list = acct_transaction['currency_type'].unique().tolist()
-currency_list.remove('USD')
+# 下載之匯率為各貨幣對美金匯率
 response = requests.get('https://tw.rter.info/capi.php')
 currency_rate = response.json()
+currency_list.remove('USD')
 rate_list = []
 for i in currency_list:
     rate_list.append(currency_rate[f'USD{i}'])
 currencies_rate = pd.DataFrame(rate_list)
 currencies_rate['currency'] = currency_list
+# 建立並計算各貨幣對台幣之匯率
 USDTWD_rate = currencies_rate.loc[currencies_rate['currency'] == 'TWD', 'Exrate'].iloc[0]
 currencies_rate['rate_to_twd'] = USDTWD_rate / currencies_rate['Exrate']
